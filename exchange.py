1f3d13410d2f3aeb84428db3aa5618cd01c4e36a
Guanchen Zhao <guanchenzhao@gmail.com>
2018-07-17 17:40:41 +0800

    面向对象

--- exchange.py
+++ exchange.py
@@ -11,29 +11,34 @@ USER = "root"
 PASSWD = "123456"
 #数据库名
 DB = "hue"
+
+# 字段、语句定义
 id_01 = -1
 id_02 = -1
-name_01 = raw_input("Please intput name_01:")
-print name_01
-name_02 = raw_input("Please intput name_02:")
-print name_02
+name_01 = "qq"
+name_02 = "ww"
+id_temp = -1 
 
-sql_check_1 = 'select id from auth_user where username = "%s";'%(name_01)
-sql_check_2 = 'select id from auth_user where username = "%s";'%(name_02)
+def set_variable():
+	global name_01
+	global name_02
+	name_01 = raw_input("Please intput name_01:")
+	print name_01
+	name_02 = raw_input("Please intput name_02:")
+	print name_02
 
 
+# sql_check_1 = 'select id from auth_user where username = "%s";'%(name_01)
+# sql_check_2 = 'select id from auth_user where username = "%s";'%(name_02)
 
 
+# 连接数据库
+def get_connection():
 
-# 打开数据库连接
-db=MySQLdb.connect(HOST,USER,PASSWD,DB)
-# 获取操作游标
-cursor=db.cursor()
-# command_a = "select id from auth_user"
-# cursor.execute(command_a)
-# # 提交到数据库执行
-# db.commit()
-# # 关闭数据库连接
+	db = MySQLdb.connect(HOST,USER,PASSWD,DB)
+	cursor = db.cursor()
+
+
 
 # name = zhaoguanchen
 
@@ -59,60 +64,71 @@ cursor=db.cursor()
 # 	print "fname=%s" % (fname)
 
 # 查找原账户ID
+def find_id(name):
 
-try:
-#    # 执行SQL语句	
-	res = cursor.execute(sql_check_1)
-	while res == 0:
-		print "原账户未找到，请重新输入0"
-		name_01 = raw_input("Please intput name_01:")
-		print name_01
-		sql_check_1 = 'select id from auth_user where username = "%s";'%(name_01)
-		res = cursor.execute(sql_check_1)
-		print sql_check_1
-	# db.commit()
+	try:
+	#    # 执行SQL语句	
+		sql_check = 'select id from auth_user where username = "%s";'%(name)
+		print sql_check
+		res = cursor.execute(sql_check)
+		while res == 0:
+			print "账户未找到，请重新输入0"
+			name_01 = raw_input("Please intput name_01:")
+			print name_01
+			
+			res = cursor.execute(sql_check)
+			# print sql_check_1
 		
-#获取所有记录列表
-	results = cursor.fetchall()
+	#获取所有记录列表
+		results = cursor.fetchall()
 
-	
-	for row in results:
-		fname = row[0]   
-		print "%s : %s" % (name_01, fname)
-		id_01 = fname
 		
+		for row in results:
+			fname = row[0]   
+			global id_temp
+			print "%s : %s" % (name_01, fname)
+			id_temp= fname
+			
+	   
+	except:
+	    print "Error: unable to fecth data with name_01"
+
+
+	return id_temp
+
+
+
+
+
+
+
+# # 查找目标账户ID
+
+# try:
+# #    # 执行SQL语句	
+# 	res = cursor.execute(sql_check_2)
+# 	while res == 0:
+# 		print "目标账户未找到，请重新输入0"
+# 		name_02 = raw_input("Please intput name_02:")
+# 		print name_02
+# 		sql_check_2 = 'select id from auth_user where username = "%s";'%(name_02)
+# 		res = cursor.execute(sql_check_2)
+# 	# db.commit()
+
+# #获取所有记录列表
+# 	results = cursor.fetchall()
+# 	for row in results:
+# 		fname = row[0]   
+# 		print "%s : %s" % (name_02, fname)
+# 		id_02 = fname
    
-except:
-    print "Error: unable to fecth data with name_01"
-
-# 查找目标账户ID
-
-try:
-#    # 执行SQL语句	
-	res = cursor.execute(sql_check_2)
-	while res == 0:
-		print "目标账户未找到，请重新输入0"
-		name_02 = raw_input("Please intput name_02:")
-		print name_02
-		sql_check_2 = 'select id from auth_user where username = "%s";'%(name_02)
-		res = cursor.execute(sql_check_2)
-	# db.commit()
-
-#获取所有记录列表
-	results = cursor.fetchall()
-	for row in results:
-		fname = row[0]   
-		print "%s : %s" % (name_02, fname)
-		id_02 = fname
-   
-except:
-    print "Error: unable to fecth data with name_02"
+# except:
+#     print "Error: unable to fecth data with name_02"
 
 
-if id_01 == -1:
-	print "原账户未找到，请重新输入"
-print id_01
-print id_02
+
+
+
 # sql_wf = 'update desktop_document2 set owner_id=74 where owner_id=57 and type="oozie-workflow2";'
 # sql_coo = 'update desktop_document2 set owner_id=74 where owner_id=57 and type="oozie-coordinator2";'
 # sql_extra = 'UPDATE desktop_document as a INNER JOIN desktop_document2 as b set a.owner_id=74 where a.object_id =b.id and b.owner_id=74;'
@@ -120,5 +136,44 @@ print id_02
 
 
 
+# def generate_html(from_someone, to_someone, words, title="love"):
+#     """
+#     :param from_someone: your name as type string
+#     :param to_someone:  your girlfriend's/boyfriend's name
+#     :param words: what you want to say
+#     :param title: the html page title
+#     :return: None. But a html file will be generate
+#     """
+#     html = open('html/template.html', encoding='UTF-8').read()
+#     pixel_matrix = text2pixel(words, width=140, height=30)
+#     six_nine = matrix2six_nine(pixel_matrix)
+
+#     html = html.replace('Your_name', from_someone)
+#     html = html.replace('Somebody', to_someone)
+#     html = html.replace('Generate text and fill in here!', six_nine)
+#     html = html.replace('Replace title here', title)
+
+#     with open('html/main.html', 'w', encoding='utf-8') as f:
+#         f.write(html)
+#         f.close()
+
+
+# if __name__ == '__main__':
+
+#     # generate_html(from_someone, to_someone, words)
+#     generate_html("Peichun", "You", "Nice to meet you!", title="hello")
+
+
+
+if __name__ == '__main__':
+	print 'aaaaaaa'
+	set_variable()
+	print name_01
+	print name_02
+	get_connection()
+	id_01 = find_id(name_01)
+	id_02 = find_id(name_02)
+	print id_01
+	print id_02
 
 
